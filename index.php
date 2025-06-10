66dc06a40c2da85ed0c11a1b0613f21042490c8d
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2025-06-10 19:45:12 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <?php
+    echo "<h1>Welcome to webapp01</h1>";
+      echo "<h1>this is 2nd line</h1>";
+    ?>
+    <h1>This is created using azure app service</h1>
+  </body>
+</html>
